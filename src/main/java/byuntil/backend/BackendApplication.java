411f65698bf8ae8acfd57d3399c9c6bd10c4b6f3
Flre-fly <66842566+Flre-fly@users.noreply.github.com>
2022-05-03 17:06:24 +0900

    * feat : login기능을 위한 기능 구현 중 - USER권한을 가진 사용자에게만 허용되는 페이지, 모든 사용자에게 허용되는 페이지를 나누어서 구현해봄
    * test : 비밀번호를 인코딩 한 후 암호화된 비밀버놓와 입력한 비밀번호가 맞는지 test

--- src/main/java/byuntil/backend/BackendApplication.java
+++ src/main/java/byuntil/backend/BackendApplication.java
@@ -5,7 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(
+        //이부분을 추가하면 appiication실행 시 error가 뜨지 않게 된다
+        exclude = {
+                org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+                org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+                org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
+        }
+)
 public class BackendApplication {
 
     public static void main(String[] args) {
